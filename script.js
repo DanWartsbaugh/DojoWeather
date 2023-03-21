97a42e98f5212a3021ff0c480174327da632bbdd
DanWartsbaugh <d.wartsbaugh@gmail.com>
2023-03-21 11:47:58 -0500

    cleaned up conversion function using querySelectorAll

--- script.js
+++ script.js
@@ -13,41 +13,21 @@ function hide(){
 var tempsC=[24,18,27,19,21,16,26,21];
 var tempsF=[75,65,80,66,69,61,78,70];
 
-//assigns variables to the temps on the html
-var a=document.querySelector("#h1")
-var b=document.querySelector("#l1")
-var c=document.querySelector("#h2")
-var d=document.querySelector("#l2")
-var e=document.querySelector("#h3")
-var f=document.querySelector("#l3")
-var g=document.querySelector("#h4")
-var h=document.querySelector("#l4")
 
-//function changes text on page between celsius and fahrenheit
-function changeScale(element){
-    //changes each vaule from celsius to fahrenheit
-    //by replacing the text with the corresponding value from tempsF
-    if(element.value=="fahr"){
-    a.innerText = tempsF[0]+"°";
-    b.innerText = tempsF[1]+"°";
-    c.innerText = tempsF[2]+"°";
-    d.innerText = tempsF[3]+"°";
-    e.innerText = tempsF[4]+"°";
-    f.innerText = tempsF[5]+"°";
-    g.innerText = tempsF[6]+"°";
-    h.innerText = tempsF[7]+"°";
-    }
+//Selects all temps and converts them between celsius and fahrenheit
+function convert(element){
+    //selects all temps
+    var temps=document.querySelectorAll(".tempNum");
+    //applies action to each item
+    temps.forEach((item) => {
+        //convert to fahrenheit by applying conversion formula and replacing text
+        if(element.value=="fahr")
+        item.innerText=Math.round(item.innerText*1.8+32);
 
-    //changes each value from fahrenheit to celsius
-        //by replacing the text with the corresponding value from tempsC
-    if(element.value=="cel"){
-        a.innerText = tempsC[0]+"°";
-        b.innerText = tempsC[1]+"°";
-        c.innerText = tempsC[2]+"°";
-        d.innerText = tempsC[3]+"°";
-        e.innerText = tempsC[4]+"°";
-        f.innerText = tempsC[5]+"°";
-        g.innerText = tempsC[6]+"°";
-        h.innerText = tempsC[7]+"°";
-        }
+        //convert to celsius by applying conversion formula and replacing text
+        if(element.value=="cel")
+        item.innerText = Math.round((item.innerText-32)*.5556)
+    })
+        
+    
 }
